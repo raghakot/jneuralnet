db088f89e901a68daa8baa86ed05df7b7f177368
ragha_da_gr8 <ragha_da_gr8@69d568dd-f712-ce2e-fc9c-804bed63deba>
2009-06-16 17:33:46 +0000

    Added BipolarSigmoid plugin, substance lnf wrapper. Added Context sensitive menus to save and load networks

---
+++ trunk/JNNHelperNB/UserInterface/src/org/jnnhelper/ui/Installer.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jnnhelper.ui;
+
+import javax.swing.UIManager;
+import org.openide.modules.ModuleInstall;
+
+/**
+ * Manages a module's lifecycle. Remember that an installer is optional and
+ * often not needed at all.
+ */
+public class Installer extends ModuleInstall
+{
+    @Override
+    public void restored() {
+        try {
+            UIManager.setLookAndFeel(new org.jvnet.substance.SubstanceLegacyDefaultLookAndFeel());
+        } catch (Exception e) {
+            //ignore...
+        }
+    }
+}
